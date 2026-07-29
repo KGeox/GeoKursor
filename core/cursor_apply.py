229e3aa1401a5c62e591c3963365fa88fe48ef43
Georges <georgeskuetche93@gmail.com>
2026-07-29 09:21:09 +0100

    making it to apply one arrow cursor from one of my customs

--- core/cursor_apply.py
+++ core/cursor_apply.py
@@ -1,2 +1,9 @@
+import ctypes
 
-import os
+def apply_cursor(path):
+    Normal =32512
+    new_cursor = ctypes.windll.user32.LoadCursorFromFileW(path)
+
+    ctypes.windll.user32.SetSystemCursor(new_cursor, Normal)
+
+apply_cursor(r"C:\Users\HP\PycharmProjects\GeoKursor\data\cursorpacks\Hollow Knight\Arrow.cur")
